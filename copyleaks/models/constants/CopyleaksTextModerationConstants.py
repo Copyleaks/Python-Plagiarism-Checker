ca2a8a1abbc85e058c35cda2657aeebbc25f5d2b
rami-amasha-copyleaks <ramia@copyleaks.com>
2025-08-28 13:05:44 +0300

    add constants for text moderation labels
    add constants for text moderation labels and for the languages

---
+++ copyleaks/models/constants/CopyleaksTextModerationConstants.py
@@ -0,0 +1,39 @@
+'''
+ The MIT License(MIT)
+
+ Copyright(c) 2016 Copyleaks LTD (https://copyleaks.com)
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+'''
+
+class CopyleaksTextModerationConstants:
+    """
+    Provides a collection of constants representing different text moderation categories
+    supported by the Copyleaks Text Moderation API.
+    """
+    ADULT_V1 = "adult-v1"
+    TOXIC_V1 = "toxic-v1"
+    VIOLENT_V1 = "violent-v1"
+    PROFANITY_V1 = "profanity-v1"
+    SELF_HARM_V1 = "self-harm-v1"
+    HARASSMENT_V1 = "harassment-v1"
+    HATE_SPEECH_V1 = "hate-speech-v1"
+    DRUGS_V1 = "drugs-v1"
+    FIREARMS_V1 = "firearms-v1"
+    CYBERSECURITY_V1 = "cybersecurity-v1"
--- example.py
+++ example.py
@@ -27,6 +27,8 @@ import random
 from copyleaks.copyleaks import Copyleaks
 from copyleaks.exceptions.command_error import CommandError
 from copyleaks.models.TextModeration.Requests.CopyleaksTextModerationRequestModel import CopyleaksTextModerationRequestModel
+from copyleaks.models.constants.CopyleaksTextModerationConstants import CopyleaksTextModerationConstants
+from copyleaks.models.constants.CopyleaksTextModerationLanguages import CopyleaksTextModerationLanguages
 from copyleaks.models.submit.ai_detection_document import NaturalLanguageDocument
 from copyleaks.models.submit.document import FileDocument, UrlDocument, OcrFileDocument
 from copyleaks.models.submit.properties.scan_properties import ScanProperties
@@ -183,19 +185,18 @@ print(response)
 model = CopyleaksTextModerationRequestModel(
     text="This is some text to scan.",
     sandbox=True,
-    language="en",
+    language=CopyleaksTextModerationLanguages.ENGLISH,
     labels=[
-        {"id": "other-v1"},
-        {"id": "adult-v1"},
-        {"id": "toxic-v1"},
-        {"id": "violent-v1"},
-        {"id": "profanity-v1"},
-        {"id": "self-harm-v1"},
-        {"id": "harassment-v1"},
-        {"id": "hate-speech-v1"},
-        {"id": "drugs-v1"},
-        {"id": "firearms-v1"},
-        {"id": "cybersecurity-v1"},
+    {"id": CopyleaksTextModerationConstants.ADULT_V1},
+    {"id": CopyleaksTextModerationConstants.TOXIC_V1},
+    {"id": CopyleaksTextModerationConstants.VIOLENT_V1},
+    {"id": CopyleaksTextModerationConstants.PROFANITY_V1},
+    {"id": CopyleaksTextModerationConstants.SELF_HARM_V1},
+    {"id": CopyleaksTextModerationConstants.HARASSMENT_V1},
+    {"id": CopyleaksTextModerationConstants.HATE_SPEECH_V1},
+    {"id": CopyleaksTextModerationConstants.DRUGS_V1},
+    {"id": CopyleaksTextModerationConstants.FIREARMS_V1},
+    {"id": CopyleaksTextModerationConstants.CYBERSECURITY_V1},
     ]
 )
 
